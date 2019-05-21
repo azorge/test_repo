44b79684cb95128c12a2270b6ecb02c33b2f3eda
Alexey Vitsenko <vits.alex@gmail.com>
2019-05-21 23:01:52 +0300

    positive and negative cases for login admin panel

--- base_tests/opencart_testing/conftest.py
+++ base_tests/opencart_testing/conftest.py
@@ -6,45 +6,53 @@ from selenium.webdriver.firefox.options import Options as FFOpts
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default='chrome', type=str)
-    parser.addoption("--url", action="store", default='10.211.55.12', type=str)
+	parser.addoption("--browser", action="store", default='chrome', type=str)
+	parser.addoption("--url", action="store", default='10.211.55.12', type=str)
 
 
 @pytest.fixture()
 def get_url(request):
-    return request.config.getoption('--url')
+	return request.config.getoption('--url')
 
 
 @pytest.fixture()
 def get_driver(request):
-    browser = request.config.getoption('--browser')
-    results_dir = path.join(getcwd(), 'results')
+	browser = request.config.getoption('--browser')
+	results_dir = path.join(getcwd(), 'results')
 
-    if not path.exists(results_dir):
-        makedirs(results_dir)
+	if not path.exists(results_dir):
+		makedirs(results_dir)
 
-    log_path = path.join(results_dir, 'driver.log')
-    drivers_dir = path.join(getcwd(), 'drivers')
+	log_path = path.join(results_dir, 'driver.log')
+	drivers_dir = path.join(getcwd(), 'drivers')
 
-    driver = None
-    driver_name = dict(
-        firefox='geckodriver',
-        chrome='chromedriver'
-    )[browser]
-    driver_path = path.join(drivers_dir, driver_name)
+	driver = None
+	driver_name = dict(
+		firefox='geckodriver',
+		chrome='chromedriver'
+	)[browser]
+	driver_path = path.join(drivers_dir, driver_name)
 
-    if browser == 'firefox':
-        options = FFOpts()
-        options.headless = True
-        driver = webdriver.Firefox(executable_path=driver_path, service_log_path=log_path, options=options)
-    elif browser == 'chrome':
-        options = ChrOpts()
-        options.add_argument("headless")
-        driver = webdriver.Chrome(executable_path=driver_path, service_log_path=log_path, options=options)
-    driver.maximize_window()
+	if browser == 'firefox':
+		options = FFOpts()
+		options.headless = True
+		driver = webdriver.Firefox(
+			executable_path=driver_path,
+			service_log_path=log_path,
+			options=options
+		)
+	elif browser == 'chrome':
+		options = ChrOpts()
+		options.add_argument("headless")
+		driver = webdriver.Chrome(
+			executable_path=driver_path,
+			service_log_path=log_path,
+			options=options
+		)
+	driver.maximize_window()
 
-    def close_driver():
-        driver.quit()
+	def close_driver():
+		driver.quit()
 
-    request.addfinalizer(close_driver)
-    return driver
+	request.addfinalizer(close_driver)
+	return driver
--- base_tests/opencart_testing/test_opencart.py
+++ base_tests/opencart_testing/test_opencart.py
@@ -10,6 +10,7 @@
 что мы находимся именно на странице приложения opencart.
 """
 
+
 # drivers_path = "/Users/alexeyvitsenko/Documents/edu/test_repo/base_tests/" \
 #                "opencart_testing/drivers/"
 
@@ -38,8 +39,34 @@
 #     assert driver.title == 'Your Store'
 #     driver.close()
 
+CREDS = {
+	'positive': ('azorge', '1q2w3e4r'),
+	'negative': ('admin', 'admin')
+}
+
 
 def test_is_opencart(get_driver, get_url):
-    driver = get_driver
-    driver.get('http://{}/opencart'.format(get_url))
-    assert driver.title == 'Your Store'
+	driver = get_driver
+	driver.get('http://{}/opencart'.format(get_url))
+	assert driver.title == 'Your Store'
+
+
+def _prep_env(driver, url, case):
+	driver.get('http://{}/opencart/admin/'.format(url))
+	login, password = CREDS[case]
+	start_title = driver.title
+	driver.find_element_by_id('input-username').send_keys(login)
+	driver.find_element_by_id('input-password').send_keys(password)
+	driver.find_element_by_class_name('btn-primary').click()
+	return start_title, driver.title
+
+
+def test_admin_panel_pos(get_driver, get_url):
+	start, end = _prep_env(get_driver, get_url, 'positive')
+	assert end != start
+
+
+def test_admin_panel_neg(get_driver, get_url):
+	start, end = _prep_env(get_driver, get_url, 'negative')
+	assert end == start
+
